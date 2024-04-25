ee13c6d55033dfb8d9ad67f29bef4c9cd041b920
wiseright <allencavaliereoscuro@gmail.com>
2024-04-25 18:35:10 +0200

    added function show_min

--- test.py
+++ test.py
@@ -3,5 +3,8 @@ lista = [1, 2, 3]
 def show_max(lista:list) -> float:
     print(f"Massimo in lista: {max(lista)}")
 
+def show_min(lista: list) -> float:
+    print(f"Minimo in lista: {min(lista)}")
+
 print(f"Lunghezza della lista: {len(lista)}")
 
